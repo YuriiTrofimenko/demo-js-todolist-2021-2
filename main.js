3128e1571fb13878505bb400f8ee73af568aa401
YuriiTrofimenko <tyaa@ukr.net>
2021-06-10 19:04:06 +0300

    edit item

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import Todo from './todo.js'
-import { state } from './store.js'
+import { state, updateItem, getItemById } from './store.js'
 
 const addItemFab = document.getElementById('fab')
 const $saveModal =
@@ -9,7 +9,9 @@ const formSubmitEvent = new Event('submit', {'cancelable': true})
 const saveModalSave = document.getElementById('saveModal__save')
 
 addItemFab.addEventListener('click', (ev) => {
+  state.selectedItemId = null
   saveModalForm.classList.remove('was-validated')
+  saveModalForm.reset()
 })
 
 saveModalSave.addEventListener('click', (ev) => {
@@ -21,7 +23,14 @@ saveModalForm.addEventListener('submit', (ev) => {
   if (saveModalForm.checkValidity()) {
     const saveModalFormTitle = document.querySelector('#saveModal form #title')
     const saveModalFormDescription = document.querySelector('#saveModal form #description')
-    state.items.unshift(new Todo(saveModalFormTitle.value, saveModalFormDescription.value, new Date()))
+    if (!state.selectedItemId) {
+      state.items.unshift(new Todo(saveModalFormTitle.value, saveModalFormDescription.value, new Date()))
+    } else {
+      updateItem({
+        'title': saveModalFormTitle.value,
+        'description': saveModalFormDescription.value
+      })
+    }
     saveModalForm.classList.remove('was-validated')
     saveModalForm.reset()
     $saveModal.hide()
@@ -47,7 +56,16 @@ const doneToggler = function (id) {
 }
 
 const saveModal = function (id) {
-    console.log('saveModal call, model id = ' + id)
+    // console.log('saveModal call, model id = ' + id)
+    state.selectedItemId = id
+    const saveModalFormTitle = document.querySelector('#saveModal form #title')
+    const saveModalFormDescription = document.querySelector('#saveModal form #description')
+    const selectedItem = getItemById(id)
+    if (selectedItem) {
+      saveModalFormTitle.value = selectedItem.title
+      saveModalFormDescription.value = selectedItem.description
+      $saveModal.show()
+    }
 }
 
 const deleteModal = function (id) {
@@ -72,7 +90,7 @@ document.addEventListener('click', ev => {
 function fillItems () {
     const cardsHtml = state.items.map(item => {
        return `<div class="col-12 col-sm-6 col-lg-4 col-xl-3 mt-3">
-                <div data-id="${item.id}" class="card" style="width: 18rem;${item.done ? 'background-color:green' : ''}">
+                <div data-id="${item.id}" class="card" style="width: 20rem;${item.done ? 'background-color:green' : ''}">
                     <div class="card-body">
                     <h5 class="card-title">${item.title}</h5>
                     <h6 class="card-subtitle mb-2 text-muted">${item.date.toLocaleString('ru').slice(0, 10)}</h6>
@@ -80,7 +98,7 @@ function fillItems () {
                     <button data-id="${item.id}" data-action="details" type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#details-modal">Details</button>
                     <button data-id="${item.id}" data-action="edit" type="button" class="btn btn-outline-secondary">Edit</button>
                     <button data-id="${item.id}" data-action="toggle-done"  type="button" class="btn btn-outline-secondary">Done</button>
-                    <button data-id="${item.id}" data-action="delete" type="button" class="btn btn-outline-secondary">Delete</button>
+                    <button data-id="${item.id}" data-action="delete" type="button" class="btn btn-outline-danger">Delete</button>
                     </div>
                 </div>
             </div>`
--- store.js
+++ store.js
@@ -6,6 +6,16 @@ const state = {
         new Todo('Todo title 3', '3Some quick example text to build on the card title and make up the bulk of the card\'s content. Some quick example text to build on the card title and make up the bulk of the card\'s content.', new Date('2021-06-12')),
         new Todo('Todo title 4', '4Some quick example text to build on the card title and make up the bulk of the card\'s content. Some quick example text to build on the card title and make up the bulk of the card\'s content.', new Date('2021-07-01')),
         new Todo('Todo title 5', '5Some quick example text to build on the card title and make up the bulk of the card\'s content.', new Date('2021-06-20'))
-    ]
+    ],
+    selectedItemId: null
 }
-export { state }
+function getItemById (id) {
+  return state.items.find(item => item.id === id)
+}
+function updateItem (payload) {
+  const selectedItem = getItemById(state.selectedItemId)
+  if (selectedItem) {
+    Object.assign(selectedItem, payload)
+  }
+}
+export { state, updateItem, getItemById }
